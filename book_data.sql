05edb0b258508266905671963ed2869421e46b9a
Chris Filkins <filchyboy@gmail.com>
2020-08-14 16:03:55 -0700

    More date/time information

--- book_data.sql
+++ book_data.sql
@@ -55,3 +55,14 @@ INSERT INTO items(price) VALUES(298.9999);
 INSERT INTO items(price) VALUES(1.9999);
  
 SELECT * FROM items;
+
+
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+ 
+SELECT * FROM people;
