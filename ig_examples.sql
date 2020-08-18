8e4bac6c35dfbfc56a4e7564add91d7022239ad4
Chris Filkins <filchyboy@gmail.com>
2020-08-18 10:57:27 -0700

    Note about WHERE before GROUP BY.

--- ig_examples.sql
+++ ig_examples.sql
@@ -98,3 +98,20 @@ INNER JOIN users
 GROUP BY photos.id
 ORDER BY total DESC
 LIMIT 1;
+
+-- 5. Calculate average number of photos per user
+
+SELECT (SELECT Count(*) 
+        FROM   photos) / (SELECT Count(*) 
+                          FROM   users) AS avg; 
+
+-- 6. Find the five most popular hashtags
+
+SELECT tags.tag_name, 
+       Count(*) AS total 
+FROM   photo_tags 
+       JOIN tags 
+         ON photo_tags.tag_id = tags.id 
+GROUP  BY tags.id 
+ORDER  BY total DESC 
+LIMIT  5;
